0a78fac7b34704472b985edd34ce212fe683f6ff
jazckson <8786660+jacksonzck@users.noreply.github.com>
2023-11-19 17:10:29 -0600

    Scrape_Before is working great!

--- commands/scrape.js
+++ commands/scrape.js
@@ -1,10 +1,13 @@
-const { SlashCommandBuilder, CommandInteraction } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, ContextMenuCommandInteraction, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { ChannelsAPI } = require('@discordjs/core')
+const { REST } = require('@discordjs/rest')
 const fs = require('fs')
 
 module.exports = {
     data: new SlashCommandBuilder()
 	.setName('scrape-channel')
     .setDescription("TODO")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
     /**
      * 
--- commands/scrape_before.js
+++ commands/scrape_before.js
@@ -1,46 +1,48 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType, ContextMenuCommandInteraction, MessageContextMenuCommandInteraction } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, ContextMenuCommandInteraction, MessageContextMenuCommandInteraction, PermissionFlagsBits } = require('discord.js');
+const { ChannelsAPI } = require('@discordjs/core')
+const { REST } = require('@discordjs/rest')
 const fs = require('fs')
 
 module.exports = {
     data: new ContextMenuCommandBuilder()
 	.setName('Scrape Before This Message')
 	.setType(ApplicationCommandType.Message)
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
     /**
      * 
      * @param {MessageContextMenuCommandInteraction} interaction 
      */
     async execute(interaction) {
-        // Check if we're in a thread in the first place.
-        if(!interaction.channel.isThread()) {
-            await interaction.reply({content: "Discord Thread Pinner only can pin messages in threads.", ephemeral: true})
-            return
+        // Make the directory if it doesn't currently exist
+        await interaction.deferReply({ephemeral: true})
+        guildName = interaction.guild.name
+        if(!fs.existsSync("data/" + guildName + "/")) {
+            fs.mkdirSync("data/" + guildName + "/")
         }
-        // Check if this is a registered channel.
-        const filepath = "data/" + interaction.guildId + ".json"
+        // Make the JSON if it doesn't exist
+        filepath = "data/" + guildName + "/" + interaction.channel.name + ".json"
         if(!fs.existsSync(filepath)) {
-            await interaction.reply({content: "Channel is unregistered for thread pinning.", ephemeral: true})
-            return
+            messages = []
+            messageJSON = {messages}
+            fs.writeFileSync(filepath, JSON.stringify(messageJSON))
         }
-        guildInformation = JSON.parse(fs.readFileSync(filepath))
-        if (! (guildInformation[interaction.channel.parentId] == true)) {
-            await interaction.reply({content: "Channel is unregistered for thread pinning.", ephemeral: true})
-            return
+        messageJSON = JSON.parse(fs.readFileSync(filepath))
+        messages = messageJSON.messages
+        // Gonna have to get the messages we want!
+        const rest = new REST({ version: '10'}).setToken(process.env.TOKEN)
+        const API = new ChannelsAPI(rest)
+        new_messages = await API.getMessages(interaction.channelId, {before: interaction.targetId, limit: 100})
+        messages.push(...new_messages)
+        while(new_messages.length == 100) {
+            await new Promise(r => setTimeout(r, 100)); // Don't want to get timed-out!
+            last_message = new_messages[99].id
+            new_messages = await API.getMessages(interaction.channelId, {before: last_message, limit: 100})
+            messages.push(...new_messages)
         }
-        // Check if the user is the original poster of the thread. 
-        if(interaction.user.id != interaction.channel.ownerId) {
-            await interaction.reply({content: "This command can only be done by the owner of the thread.", ephemeral: true})
-            return
-        }
-        // Check to see if the message is already pinned. If so unpin it, if not pin it.
-        if(!interaction.targetMessage.pinned) {
-            await interaction.reply({content: "Pinning message.", ephemeral: true})
-            await interaction.targetMessage.pin()
-            return
-        } else {
-            await interaction.reply({content: "Unpinning message.", ephemeral: true})
-            await interaction.targetMessage.unpin()
-            return
-        }
+        messageJSON.messages = messages
+        console.log("" + messages.length + " messages scraped.")
+        fs.writeFileSync(filepath, JSON.stringify(messageJSON))
+        await interaction.followUp({content: "Done!", ephemeral: true})
     },
 }
