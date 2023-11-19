ceb627a9a2f872a2b78dfbcf1d776d1ab611d3b0
jazckson <8786660+jacksonzck@users.noreply.github.com>
2023-11-19 17:14:08 -0600

    Badabing badaboom

--- commands/scrape.js
+++ commands/scrape.js
@@ -1,4 +1,4 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType, ContextMenuCommandInteraction, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { CommandInteraction, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { ChannelsAPI } = require('@discordjs/core')
 const { REST } = require('@discordjs/rest')
 const fs = require('fs')
@@ -6,7 +6,7 @@ const fs = require('fs')
 module.exports = {
     data: new SlashCommandBuilder()
 	.setName('scrape-channel')
-    .setDescription("TODO")
+    .setDescription("Scrapes the channel, and outputs it into a JSON file into the bot's directory")
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
     /**
@@ -14,7 +14,35 @@ module.exports = {
      * @param {CommandInteraction} interaction 
      */
     async execute(interaction) {
-        await interaction.reply("Todo");
-        return
+        // Make the directory if it doesn't currently exist
+        await interaction.deferReply({ephemeral: true})
+        guildName = interaction.guild.name
+        if(!fs.existsSync("data/" + guildName + "/")) {
+            fs.mkdirSync("data/" + guildName + "/")
+        }
+        // Make the JSON if it doesn't exist
+        filepath = "data/" + guildName + "/" + interaction.channel.name + ".json"
+        if(!fs.existsSync(filepath)) {
+            messages = []
+            messageJSON = {messages}
+            fs.writeFileSync(filepath, JSON.stringify(messageJSON))
+        }
+        messageJSON = JSON.parse(fs.readFileSync(filepath))
+        messages = messageJSON.messages
+        // Gonna have to get the messages we want!
+        const rest = new REST({ version: '10'}).setToken(process.env.TOKEN)
+        const API = new ChannelsAPI(rest)
+        new_messages = await API.getMessages(interaction.channelId, {before: interaction.channel.lastMessageId, limit: 100})
+        messages.push(...new_messages)
+        while(new_messages.length == 100) {
+            await new Promise(r => setTimeout(r, 100)); // Don't want to get timed-out!
+            last_message = new_messages[99].id
+            new_messages = await API.getMessages(interaction.channelId, {before: last_message, limit: 100})
+            messages.push(...new_messages)
+        }
+        messageJSON.messages = messages
+        console.log("" + messages.length + " messages scraped.")
+        fs.writeFileSync(filepath, JSON.stringify(messageJSON))
+        await interaction.followUp({content: "Done!", ephemeral: true})
     },
 }
